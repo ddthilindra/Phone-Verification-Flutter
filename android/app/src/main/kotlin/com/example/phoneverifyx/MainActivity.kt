ba1d5d4ef762d9302b0548179db9851b928d9c7d
ddthilindra <59472990+ddthilindra@users.noreply.github.com>
2022-04-22 17:40:53 +0530

    Completed

---
+++ android/app/src/main/kotlin/com/example/phoneverifyx/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.phoneverifyx
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,96 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:phoneverifyx/loginScreen.dart';
+
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: const LoginScreen()
+    );
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class MyApp extends StatelessWidget {
+//   // This widget is the root of your application.
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//         title: 'Flutter Demo',
+//         theme: ThemeData(
+//           primarySwatch: Colors.blue,
+//           visualDensity: VisualDensity.adaptivePlatformDensity,
+//         ),
+//         debugShowCheckedModeBanner: false,
+//         home: InitializerWidget());
+//   }
+// }
+
+// class InitializerWidget extends StatefulWidget {
+//   @override
+//   _InitializerWidgetState createState() => _InitializerWidgetState();
+// }
+
+// class _InitializerWidgetState extends State<InitializerWidget> {
+
+//   FirebaseAuth _auth;
+
+//   User _user;
+
+//   bool isLoading = true;
+
+
+//   @override
+//   void initState() {
+//     // TODO: implement initState
+//     super.initState();
+//     _auth = FirebaseAuth.instance;
+//     _user = _auth.currentUser;
+//     isLoading = false;
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return isLoading ? Scaffold(
+//       body: Center(
+//         child: CircularProgressIndicator(),
+//       ),
+//     ) : _user == null ? LoginScreen() : HomeScreen();
+//   }
+// }
